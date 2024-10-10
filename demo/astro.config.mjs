8ebe0572abaa856532ac6edc5ac774351bbcbe20
salvadorsru <salvadorsru@gmail.com>
2024-10-10 19:46:31 +0200

    fix(astro.config): removed unnecesary vite config

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,14 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import path from "node:path"
 
 // https://astro.build/config
-export default defineConfig({
-    vite: {
-        resolve: {
-            // alias: {
-            //     'searchable-select': path.resolve('./node_modules/@salvadorsru/searchable-select/dist')
-            // }
-        }
-    }
-});
+export default defineConfig({});
