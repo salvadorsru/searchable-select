0a86ea3634ce93edd5401fb0e9db3217e68c250a
salvadorsru <salvadorsru@gmail.com>
2024-10-24 23:21:42 +0200

    feat(build): added directly compilation for demo usage

--- src/build.js
+++ src/build.js
@@ -13,6 +13,18 @@ try {
         minify: true,
         outfile: 'dist/style.min.css',
     })
+    await esbuild.build({
+        entryPoints: ['src/component/index.js'],
+        bundle: true,
+        minify: true,
+        outfile: 'demo/public/js/index.min.js',
+    })
+
+    await esbuild.build({
+        entryPoints: ['src/component/style.css'],
+        minify: true,
+        outfile: 'demo/public/css/style.min.css',
+    })
 } catch {
     process.exit(1)
 }
